d149ab45896c5db724f0a715b7e952c3c1bde76c
tianli <zengtianli1@gmail.com>
2026-04-19 10:09:49 +0800

    sync: 2026-04-19 10:09

--- lib/catalog.generated.ts
+++ lib/catalog.generated.ts
@@ -25,7 +25,7 @@ export interface CatalogEntry {
 
 export const CATALOG = ({
   "meta": {
-    "generated_at": "2026-04-19T10:01:08",
+    "generated_at": "2026-04-19T10:05:21",
     "sources": [
       "~/Dev/website/lib/services.ts",
       "~/Dev/configs/menus/sites/*.yaml",
